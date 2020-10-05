f17577f4176c029cede1181a56b402e7146e349f
doif-dobby <doif.dobby@gmail.com>
2020-10-05 15:20:06 +0900

    [#8] Check 컴포넌트 완성

---
+++ src/Check/Check.stories.tsx
@@ -0,0 +1,68 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import Check from './Check';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
+import Color from '../styles/colors/Color';
+import { ChangeEvent, useCallback, useState } from 'react';
+
+export default {
+  title: 'components/Check',
+  component: Check,
+  decorators: [withKnobs],
+};
+
+export const check = () => {
+  const color = select(
+    'color',
+    [
+      Color.WHITE,
+      Color.BLACK,
+      Color.GRAY,
+      Color.PINK,
+      Color.VIOLET,
+      Color.BLUE,
+      Color.TEAL,
+      Color.LIME,
+      Color.ORANGE,
+      Color.RED,
+      Color.GRAPE,
+      Color.INDIGO,
+      Color.CYAN,
+      Color.GREEN,
+      Color.YELLOW,
+    ],
+    Color.VIOLET
+  );
+
+  const data = [
+    {
+      code: 'A',
+      name: '안녕하세요',
+    },
+    {
+      code: 'B',
+      name: '반갑습니다',
+    },
+    {
+      code: 'C',
+      name: '또 만나요',
+    },
+  ];
+
+  const [value, setValue] = useState([]);
+
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const code = e.target.dataset.code;
+    const checked = e.target.checked;
+
+    setValue((value) =>
+      checked ? value.concat(code) : value.filter((val) => val !== code)
+    );
+  }, []);
+
+  return <Check data={data} onChange={onChange} value={value} color={color} />;
+};
+
+check.story = {
+  name: 'Default',
+};
---
+++ src/Check/Check.tsx
@@ -0,0 +1,110 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import styled from '@emotion/styled';
+import Color from '../styles/colors/Color';
+import palette from '../styles/colors/palette';
+
+interface CheckProps {
+  /** check box의 data를 설정합니다. */
+  data: Array<CheckedProps>;
+  /** check box의 checked를 설정합니다. */
+  value: string[];
+  /** check box의 색을 정합니다. */
+  color: Color;
+  /** check box를 변경했을 때 트리거되는 함수입니다. */
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+interface StyledCheckProps {
+  color: Color;
+  number: number;
+}
+
+interface CheckedProps {
+  code: string;
+  name: string;
+}
+
+/**
+ * `Check` 컴포넌트는 여러 개의 값을 선택할 때 사용합니다.
+ */
+function Check(props: CheckProps) {
+  return (
+    <StyledCheck
+      color={props.color}
+      number={props.color === Color.GRAY ? 7 : 5}>
+      {props.data.map((d) => {
+        const isChecking = props.value.includes(d.code);
+
+        return (
+          <label key={d.code}>
+            <input
+              type="checkbox"
+              checked={isChecking}
+              data-code={d.code}
+              onChange={props.onChange}
+            />
+            <div className={`checkbox ${isChecking && 'checking'}`}>
+              <div className={isChecking ? 'checking' : ''}></div>
+            </div>
+            <span>{d.name}</span>
+          </label>
+        );
+      })}
+    </StyledCheck>
+  );
+}
+
+Check.defaultProps = {
+  color: Color.VIOLET,
+};
+
+const StyledCheck = styled.div`
+  display: flex;
+
+  & > label {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    width: fit-content;
+    margin-right: 0.5rem;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    & > input[type='checkbox'] {
+      display: none;
+    }
+
+    & > div.checkbox {
+      border: 1px solid ${palette.gray[7]};
+      border-radius: 4px;
+      width: 1.2rem;
+      height: 1.2rem;
+      margin-right: 0.2rem;
+      position: relative;
+
+      &.checking {
+        background: ${(props: StyledCheckProps) =>
+          palette[props.color][props.number]};
+        border: none;
+      }
+
+      & > div.checking {
+        position: absolute;
+        top: 0.2rem;
+        left: 0.25rem;
+        border-left: 0.2rem solid #fff;
+        border-bottom: 0.2rem solid #fff;
+
+        width: 0.7rem;
+        height: 0.5rem;
+
+        transform: rotate(-45deg);
+      }
+    }
+  }
+`;
+
+export default Check;
