d3b759539d2cb313bc0fd7d2d373ec54a794fd8c
doif-dobby <doif.dobby@gmail.com>
2020-10-09 18:49:11 +0900

    [#13] DateUtil에 반환 타입추가

--- src/libs/DateUtil.ts
+++ src/libs/DateUtil.ts
@@ -4,7 +4,9 @@ import _ from 'lodash';
 /**
  * date 타입만 갖고 있는 object => string 타입으로 변환 후 object로 반환
  */
-export const mapDateString = (dateObj: { [index: string]: Date | null }) => {
+export const mapDateString = (dateObj: {
+  [index: string]: Date | null;
+}): { [index: string]: string } => {
   const stringObj = _.mapValues(dateObj, (o) =>
     o ? moment(o).format('YYYY-MM-DD') : ''
   );
